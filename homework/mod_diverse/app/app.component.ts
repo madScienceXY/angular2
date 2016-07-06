47e9d42f3839a1e1b9e2a0fad151aa856a702574
1058077 <flo.ces.fc@gmail.com>
2016-07-06 02:17:10 +0200

    angular lifecycle

--- homework/mod_diverse/app/app.component.ts
+++ homework/mod_diverse/app/app.component.ts
@@ -1,8 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, AfterContentInit, AfterViewInit} from '@angular/core';
+import {ChildComponent} from './child.component';
 
 @Component({
   selector: 'demo-app',
   templateUrl: 'app/app.component.html',
-  directives:  []
+  directives:  [ChildComponent]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, AfterContentInit, AfterViewInit  {
+
+  public currentValue : number = 3;
+
+  messages : string[] = [];
+
+
+  ngOnInit() {
+    console.log('AppComponent initialized');
+    this.messages.push('OnInit');
+  }
+
+  ngAfterContentInit() {
+    this.messages.push('AfterContentInit');
+  }
+
+  ngAfterViewInit() {
+    this.messages.push('AfterViewInit');
+  }
+
+ }
