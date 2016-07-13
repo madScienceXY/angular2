87d23ca6d958972b4c4f6dd542231a89b3ba0193
Florian Cesal <flo.ces.fc@gmail.com>
2016-07-13 21:17:39 +0200

    older files

--- homework/mod_diverse/app/app.component.ts
+++ homework/mod_diverse/app/app.component.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'demo-app',
   templateUrl: 'app/app.component.html',
   directives:  []
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit() {
+      console.log('AppComponent initialized');
+  }
+ }
